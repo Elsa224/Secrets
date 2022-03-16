a55fac13d16b44426b1ba171c11810b174f357cf
Elsa Zougouri <elsazougouri224@gmail.com>
2022-03-16 00:02:24 +0000

    Level 6 - OAuth with Google - Beginning

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require( "mongoose" );
 const session = require( "express-session" );
 const passport = require( "passport" ); //using passport to add cookies and session
 const passportLocalMongoose = require( "passport-local-mongoose" );
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // OAuth with Google
 
 //Creating an app constant and use EJS as its view engine
 const app = express( );
@@ -53,6 +54,17 @@ passport.use( User.createStrategy() );
 
 passport.serializeUser( User.serializeUser() );
 passport.deserializeUser( User.deserializeUser() );
+
+passport.use( new GoogleStrategy( { 
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  }, ( accessToken, refreshToken, profile, cb ) => {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        } );
+    }
+) );
 
 //Web Routes
 app.get( "/", ( req, res ) => {
